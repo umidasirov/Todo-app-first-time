7c6009382882539d44e50f8456f266c0b00a54a1
umidasirov <126274140+umidasirov@users.noreply.github.com>
2023-06-08 16:17:33 +0300

    Add files via upload

---
+++ solve/script.js
@@ -0,0 +1,26 @@
+
+let nsf = document.getElementById('btn')
+    nsf.addEventListener('click', () =>{
+        let tr = document.createElement('tr')
+        let th = document.createElement('th')
+        let tg = document.createElement('th')
+        let tj = document.createElement('th')
+    let nam = document.getElementById('nam')
+        console.log(nam.value)
+    let author = document.getElementById('author')
+    let year = document.getElementById('year')
+    const Name = document.createTextNode(nam.value);
+    const Author = document.createTextNode(author.value);
+    const Year = document.createTextNode(year.value);
+    th.appendChild(Name)
+        tg.appendChild(Author)
+		th.classList.add('th')
+        tg.classList.add('tg')
+        tj.appendChild(Year)
+        tr.appendChild(th),tr.appendChild(tg),tr.appendChild(tj)
+    document.getElementById('tab').appendChild(tr)
+        nam.value = ''
+        year.value = ''
+        author.value = ''
+})
+console.log(a)
